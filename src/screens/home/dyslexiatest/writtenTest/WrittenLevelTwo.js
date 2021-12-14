d5e02b6b03fc9e1c6b9a8cc5f05d1dae2d579fe4
alihassan020 <alihassan4651@gmail.com>
2021-12-14 07:55:34 +0500

    backend linked complete

--- src/screens/home/dyslexiatest/writtenTest/WrittenLevelTwo.js
+++ src/screens/home/dyslexiatest/writtenTest/WrittenLevelTwo.js
@@ -14,6 +14,8 @@ import GradientView from '../../../../components/common/GradientView';
 import {COLORS, icons} from '../../../../constants';
 import Voice from '@react-native-community/voice';
 import Report from '../../../../components/dyslexicTest/Report';
+import {getWrittenTest} from '../../../../api/dyslexiaTest';
+import AppLoader from '../../../../components/common/AppLoader';
 import styles from './styles';
 import Animated, {
   useSharedValue,
@@ -22,7 +24,6 @@ import Animated, {
   Easing,
 } from 'react-native-reanimated';
 
-const data = ['Information', 'Today', 'Dog', 'Read', 'Listen'];
 const WrittenLevelTwo = ({navigation}) => {
   const [backModal, setBackModal] = useState(false);
   const [result, setResult] = useState('');
@@ -34,14 +35,28 @@ const WrittenLevelTwo = ({navigation}) => {
   const [compare, setCompare] = useState(false);
   const [report, setReport] = useState(false);
   const [speechEnd, setSpeechEnd] = useState(false);
-
+  const [fetch, setfetch] = useState(true);
   const BackScreen = navigation.goBack;
   const handleContinue = () => setBackModal(true);
+  useEffect(() => {
+    const postUserScore = async () => {
+      const test = 'Written';
+      const level = 'Level 2';
+      const data = {
+        test,
+        level,
+      };
+      const res = await getWrittenTest(data);
+      console.log(res.data[0].verbalQuestions);
+      setTest(res.data[0].verbalQuestions);
+      setfetch(false);
+    };
+    postUserScore();
+  }, []);
   useEffect(() => {
     Voice.onSpeechStart = onSpeechStartHandler;
     Voice.onSpeechEnd = onSpeechEndHandler;
     Voice.onSpeechResults = onSpeechResultsHandler;
-    setTest(data);
     return () => {
       Voice.destroy().then(Voice.removeAllListeners);
     };
@@ -108,133 +123,149 @@ const WrittenLevelTwo = ({navigation}) => {
   return (
     <BackgroundImageApp>
       <HeaderTest headerText="Level 2" BackScreen={handleContinue} />
-      <ModalApp
-        visible={backModal}
-        feedback=" Do you want quit the Written Test?"
-        quit={BackScreen}
-        continueTest={() => setBackModal(false)}
-        quitText="Go Back"
-        continueTestText="Continue"
-      />
-      <View style={styles.bodyContainer}>
-        {report ? (
-          <Report
-            test="Written"
-            resetTest={handleReset}
-            nextLevel={nextLevel}
-            obtainedScore={score}
-            totalScore={test.length}
-            level="Level 2"
+      {fetch ? (
+        <AppLoader />
+      ) : (
+        <>
+          <ModalApp
+            visible={backModal}
+            feedback=" Do you want quit the Written Test?"
+            quit={BackScreen}
+            continueTest={() => setBackModal(false)}
+            quitText="Go Back"
+            continueTestText="Continue"
           />
-        ) : (
-          <>
-            <View style={styles.instructionContainer}>
-              <Text style={styles.title}>Level 2</Text>
-              <GradientView
-                colors={[COLORS.primary, COLORS.secondary]}
-                style={styles.questionCount}>
-                <Text style={styles.subHeading}>
-                  {question + 1}/{test.length}
-                </Text>
-              </GradientView>
-              <Animated.View style={[styles.textContainer, reanimatedStyle]}>
-                <Text style={[styles.headingText, {color: COLORS.black}]}>
-                  {test[question]}
-                </Text>
-              </Animated.View>
-              <Text style={styles.paragrapgh}>
-                Press on the button blow & and read the word loud
-              </Text>
-            </View>
-            <View style={styles.speakContainer}>
-              {check ? null : (
-                <>
-                  <View style={styles.textInputStyle}>
-                    <TextInput
-                      value={result}
-                      style={[styles.paragrapgh, {color: 'black'}]}
-                      secureTextEntry
-                    />
-
-                    {isLoading ? (
-                      <ActivityIndicator size="large" color={COLORS.primary} />
-                    ) : (
-                      <TouchableOpacity
-                        onPress={startRecording}
-                        disabled={check}>
-                        <Image
-                          source={icons.mic}
-                          style={{
-                            width: 25,
-                            height: 25,
-                            tintColor: COLORS.primary,
-                          }}
-                        />
-                      </TouchableOpacity>
-                    )}
-                  </View>
-                  {speechEnd ? (
-                    <TouchableOpacity
-                      style={styles.submitBtn}
-                      onPress={stopRecording}
-                      disabled={check}>
-                      <Text style={styles.headingText}>Submit</Text>
-                    </TouchableOpacity>
-                  ) : null}
-                </>
-              )}
-              {check ? (
-                <View style={styles.resultContainer}>
-                  <Text style={styles.title}> Result</Text>
-                  <View
-                    style={{
-                      width: '45%',
-                      height: '17%',
-                      borderRadius: 20,
-                      borderWidth: 3,
-                      borderColor: compare ? COLORS.success : COLORS.error,
-                      flexDirection: 'row',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}>
-                    <Text style={[styles.subHeading]}>
-                      {compare ? 'Correct Answer' : 'Wrong Answer'}
+          <View style={styles.bodyContainer}>
+            {report ? (
+              <Report
+                test="Written"
+                resetTest={handleReset}
+                nextLevel={nextLevel}
+                obtainedScore={score}
+                totalScore={test.length}
+                level="Level 2"
+              />
+            ) : (
+              <>
+                <View style={styles.instructionContainer}>
+                  <Text style={styles.title}>Level 2</Text>
+                  <GradientView
+                    colors={[COLORS.primary, COLORS.secondary]}
+                    style={styles.questionCount}>
+                    <Text style={styles.subHeading}>
+                      {question + 1}/{test.length}
                     </Text>
-                  </View>
-                  <Text style={styles.headingText}> You speak</Text>
-                  <View
-                    style={{
-                      width: '45%',
-                      height: '17%',
-                      borderRadius: 20,
-                      borderWidth: 3,
-                      borderColor: compare ? COLORS.success : COLORS.error,
-                      flexDirection: 'row',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}>
-                    <Text style={styles.headingText}>{result}</Text>
-                  </View>
-
-                  {question < 4 ? (
-                    <TouchableOpacity
-                      style={[styles.submitBtn, {width: '30%', height: '15%'}]}
-                      onPress={handleNext}>
-                      <Text style={styles.headingText}>Next</Text>
-                    </TouchableOpacity>
-                  ) : (
-                    <TouchableOpacity
-                      style={[styles.submitBtn, {width: '35%', height: '15%'}]}
-                      onPress={() => setReport(true)}>
-                      <Text style={styles.headingText}>Get Report</Text>
-                    </TouchableOpacity>
-                  )}
+                  </GradientView>
+                  <Animated.View
+                    style={[styles.textContainer, reanimatedStyle]}>
+                    <Text style={[styles.headingText, {color: COLORS.black}]}>
+                      {test[question]}
+                    </Text>
+                  </Animated.View>
+                  <Text style={styles.paragrapgh}>
+                    Press on the button blow & and read the word loud
+                  </Text>
                 </View>
-              ) : null}
-            </View>
-          </>
-        )}
-      </View>
+                <View style={styles.speakContainer}>
+                  {check ? null : (
+                    <>
+                      <View style={styles.textInputStyle}>
+                        <TextInput
+                          value={result}
+                          style={[styles.paragrapgh, {color: 'black'}]}
+                          secureTextEntry
+                        />
+
+                        {isLoading ? (
+                          <ActivityIndicator
+                            size="large"
+                            color={COLORS.primary}
+                          />
+                        ) : (
+                          <TouchableOpacity
+                            onPress={startRecording}
+                            disabled={check}>
+                            <Image
+                              source={icons.mic}
+                              style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: COLORS.primary,
+                              }}
+                            />
+                          </TouchableOpacity>
+                        )}
+                      </View>
+                      {speechEnd ? (
+                        <TouchableOpacity
+                          style={styles.submitBtn}
+                          onPress={stopRecording}
+                          disabled={check}>
+                          <Text style={styles.headingText}>Submit</Text>
+                        </TouchableOpacity>
+                      ) : null}
+                    </>
+                  )}
+                  {check ? (
+                    <View style={styles.resultContainer}>
+                      <Text style={styles.title}> Result</Text>
+                      <View
+                        style={{
+                          width: '45%',
+                          height: '17%',
+                          borderRadius: 20,
+                          borderWidth: 3,
+                          borderColor: compare ? COLORS.success : COLORS.error,
+                          flexDirection: 'row',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}>
+                        <Text style={[styles.subHeading]}>
+                          {compare ? 'Correct Answer' : 'Wrong Answer'}
+                        </Text>
+                      </View>
+                      <Text style={styles.headingText}> You speak</Text>
+                      <View
+                        style={{
+                          width: '45%',
+                          height: '17%',
+                          borderRadius: 20,
+                          borderWidth: 3,
+                          borderColor: compare ? COLORS.success : COLORS.error,
+                          flexDirection: 'row',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}>
+                        <Text style={styles.headingText}>{result}</Text>
+                      </View>
+
+                      {question < 4 ? (
+                        <TouchableOpacity
+                          style={[
+                            styles.submitBtn,
+                            {width: '30%', height: '15%'},
+                          ]}
+                          onPress={handleNext}>
+                          <Text style={styles.headingText}>Next</Text>
+                        </TouchableOpacity>
+                      ) : (
+                        <TouchableOpacity
+                          style={[
+                            styles.submitBtn,
+                            {width: '35%', height: '15%'},
+                          ]}
+                          onPress={() => setReport(true)}>
+                          <Text style={styles.headingText}>Get Report</Text>
+                        </TouchableOpacity>
+                      )}
+                    </View>
+                  ) : null}
+                </View>
+              </>
+            )}
+          </View>
+        </>
+      )}
     </BackgroundImageApp>
   );
 };
